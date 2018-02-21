fab61d5fe059238cdd98d7064edbc526a6cace0a
1395917451 <1395917451@qq.com>
2018-02-21 23:34:34 +0800

    frist

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+import 'common/stylus/index.styl'
+
+Vue.config.productionTip = false
+FastClick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
